82458dceccd21194d684cca25b3a4482e812798d
Tiebatong <Philipprhinow@gmail.com>
2025-04-12 22:59:47 +0200

    Add files via upload

--- Aufgabe4.c
+++ Aufgabe4.c
@@ -7,10 +7,14 @@ int main(void)
     const float pi = 3.14159;
     float radius;
     float Kugel_berechnung[10]; // Durchmesser, Oberfläche, Volumen
+    int Rundungs_stellen;
     
 
     printf("Bitte geben sie hier den Radius an: \n");
     scanf("%f", &radius);
+
+    printf("Auf wie viele Stellen soll gerundet werden? \n");
+    scanf("%d", &Rundungs_stellen);
 
      
     Kugel_berechnung[0] = radius * 2;
@@ -21,9 +25,9 @@ int main(void)
      
     Kugel_berechnung[2] = (4 / 3) * pi * (radius * radius * radius);
 
-    printf("Durchmesser: %.2f\n", Kugel_berechnung[0]);
-    printf("Oberflaeche: %.2f\n", Kugel_berechnung[1]);
-    printf("Volumen: %.2f\n", Kugel_berechnung[2]);
+    printf("Durchmesser: %.*f\n", Rundungs_stellen, Kugel_berechnung[0]);
+    printf("Oberflaeche: %.*f\n", Rundungs_stellen, Kugel_berechnung[1]);
+    printf("Volumen: %.*f\n", Rundungs_stellen, Kugel_berechnung[2]);
     system("pause");
     return 0;
 }
